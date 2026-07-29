e471bad600eff2196f05e6adbc3a2f9c40c33562
sdiaz-ons <simon.diaz@ext.ons.gov.uk>
2026-07-29 10:37:24 +0100

    FMT-89 Fix GCP Credentials issue when running locally

---
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/config/CsvServicePubSubEmulatorConfig.java
@@ -0,0 +1,24 @@
+package uk.gov.ons.census.fwmt.csvservice.config;
+
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * Ensure Spring Cloud GCP Pub/Sub runs against emulator without requiring
+ * Application Default Credentials. Only active when emulator host is configured.
+ */
+@Configuration
+public class CsvServicePubSubEmulatorConfig {
+
+  @Bean
+  @Primary
+  @ConditionalOnProperty(name = "spring.cloud.gcp.pubsub.emulator-host")
+  public CredentialsProvider pubsubNoCredentialsProvider() {
+    return NoCredentialsProvider.create();
+  }
+}
+
