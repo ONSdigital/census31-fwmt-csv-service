4e71b50837901dc4af2a9664f305536975d9cb21
Kieran Wardle <23309814+KieranWardle@users.noreply.github.com>
2026-08-13 18:14:54 +0100

    FMT-118 Added Service version to OpenAPI and GAR config

--- src/main/java/uk/gov/ons/census/fwmt/csvservice/config/OpenApiConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/csvservice/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package uk.gov.ons.census.fwmt.csvservice.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -12,31 +13,35 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @Configuration
 public class OpenApiConfig {
 
+  @Value("${version}")
+  String version;
+
   @Bean
   public OpenAPI openAPI() {
 
-      String serverUrl = getServerUrl();
-      return new OpenAPI()
-      .openapi("3.2.0")
-              .info(new Info()
-                      .title("FWMT Gateway - CSV Service")
-                      .version("1.0.0"))
-              .addServersItem(new Server()
-                      .url(serverUrl)
-                      .description("Default Server"));
+    String serverUrl = getServerUrl();
+    return new OpenAPI()
+        .openapi("3.2.0")
+        .info(new Info()
+            .title("FWMT Gateway - CSV Service")
+            .version(version))
+        .addServersItem(new Server()
+            .url(serverUrl)
+            .description("Default Server"));
+  }
+
+  private String getServerUrl() {
+    try {
+      ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+      if (attributes != null) {
+        return ServletUriComponentsBuilder.fromRequestUri(attributes.getRequest())
+            .replacePath("")
+            .build()
+            .toUriString();
+      }
+    } catch (Exception e) {
+      // Fall back if no request context available
     }
-    private String getServerUrl() {
-        try {
-            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-            if (attributes != null) {
-                return ServletUriComponentsBuilder.fromRequestUri(attributes.getRequest())
-                        .replacePath("")
-                        .build()
-                        .toUriString();
-            }
-        } catch (Exception e) {
-            // Fall back if no request context available
-        }
-        return "/";
-    }
+    return "/";
+  }
 }
